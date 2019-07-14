93e3523edf89a312349a506ed4dafe3dc30d0c5e
Nemeth Tamas <tamasnemeth@Nemeth-iMac.local>
2019-07-14 22:28:59 +0200

    chore(script.js)add javaScript class along with constructor and functions number

--- script.js
+++ script.js
@@ -1,1 +1,90 @@
+  class Calculator {
+      constructor(previousOperandTextElement, currentOperandTextElement) {
+          this.previousOperandTextElement = previousOperandTextElement
+          this.currentOperandTextElement = currentOperandTextElement
+          this.clear()
+      }
+      clear() {
+          this.currentOperand = ''
+          this.previousOperand = ''
+          this.operation = undefined
+      }
+      appendNumber(number) {
+          if (number == ',' && this.currentOperand.includes(',')) return
+          this.currentOperand = this.currentOperand.toString() + number.toString()
+      }
+      selectOperation(operation) {
+          if (this.currentOperand === '') return
+          if (previousOperand !== '') {
+              this.compute()
+          }
+          this.operation = operation
+          this.previousOperand = this.currentOperand
+          this.currentOperand = ''
+      }
+      compute() {
+          let computation
+          const prev = parseFloat(this.previousOperand)
+          const current = parseFloat(this.currentOperand)
+          if (isNaN(prev) || isNaN(current)) return
 
+          switch (this.operation) {
+              case '+':
+                  computation = prev + current
+                  break
+              case '–':
+                  computation = prev - current
+                  break
+              case '*':
+                  computation = prev * current
+                  break
+              case '÷':
+                  computation = prev / current
+                  break
+              case '%':
+                  computation = prev % current
+                  break
+              case '+/-':
+                  computation = abs(computation)
+                  break
+              default:
+                  return
+          }
+          this.currentOperand = computation
+          this.operation = undefined
+          this.previousOperand = ''
+
+      }
+      updateDisplay() {
+          this.currentOperandTextElement.innerText = this.currentOperand
+          this.previousOperandTextElement.innerText = this.previousOperand
+      }
+  }
+
+  const numberButtons = document.querySelectorAll('[data-number]')
+  const operationButtons = document.querySelectorAll('[data-operation]')
+  const equalsButton = document.querySelector('[data-equals]')
+  const allClearButton = document.querySelector('[data-all-clear]')
+  const absoluteValueButton = document.querySelector('[data-absolute-value]')
+  const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+  const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+  const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+  numberButtons.forEach(button => {
+      button.addEventListener('click', () => {
+          calculator.appendNumber(button.innerText)
+          calculator.updateDisplay()
+      })
+  })
+  operationButtons.forEach(button => {
+      button.addEventListener('click', () => {
+          calculator.selectOperation(button.innerText)
+          calculator.updateDisplay()
+      })
+  })
+
+  equalsButton.addEventListener('click', button => {
+      calculator.compute()
+      calculator.updateDisplay()
+  })
